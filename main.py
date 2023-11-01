20ec60695674351eb151b12e2613b27a3c855861
Alphonce <alphoncealphaxad45@gmail.com>
2023-11-01 09:20:32 +0300

    stach
    stach

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import qrcode
+from kivy.base import EventLoop
 from kivy.graphics.svg import Window
 
 from kivymd.app import MDApp
+from kivymd.toast import toast
 
 Window.size = [420, 740]
 
@@ -27,6 +29,50 @@ class Main(MDApp):
         sm = self.root
         sm.current = "qr"
 
+
+    """ KEYBOARD INTEGRATION """
+
+    def keyboard_hooker(self, *args):
+        EventLoop.window.bind(on_keyboard=self.hook_keyboard)
+
+    def hook_keyboard(self, window, key, *largs):
+        print(self.screens_size)
+        if key == 27 and self.screens_size > 0:
+            print(f"your were in {self.current}")
+            last_screens = self.current
+            self.screens.remove(last_screens)
+            print(self.screens)
+            self.screens_size = len(self.screens) - 1
+            self.current = self.screens[len(self.screens) - 1]
+            self.screen_capture(self.current)
+            return True
+        elif key == 27 and self.screens_size == 0:
+            toast('Press Home button!')
+            return True
+
+    """ SCREEN FUNCTIONS """
+
+    def screen_capture(self, screen):
+        sm = self.root
+        sm.current = screen
+        if screen in self.screens:
+            pass
+        else:
+            self.screens.append(screen)
+        print(self.screens)
+        self.screens_size = len(self.screens) - 1
+        self.current = self.screens[len(self.screens) - 1]
+        print(f'size {self.screens_size}')
+        print(f'current screen {screen}')
+
+    def screen_leave(self):
+        print(f"your were in {self.current}")
+        last_screens = self.current
+        self.screens.remove(last_screens)
+        print(self.screens)
+        self.screens_size = len(self.screens) - 1
+        self.current = self.screens[len(self.screens) - 1]
+        self.screen_capture(self.current)
 
 
 Main().run()
